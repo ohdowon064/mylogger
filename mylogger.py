0909959290b5ff48b0c40113cac12109e80a3605
Dowon Oh <dhehdnjs03@gmail.com>
2025-09-07 01:39:55 +0900

    날짜 추가

--- mylogger.py
+++ mylogger.py
@@ -74,7 +74,8 @@ def set_logger(json_format: bool = True):
 
         logger.add(
             sys.stderr,
-            format="<level>{level: <8}</level>"
+            format="<green>{time:YYYY-MM-DD HH:mm:ss.SSSZ}</green>"
+            "<level>{level: <8}</level>"
             " | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>"
             " - <level>{message}</level>"
             " - <level>{extra}</level>",
